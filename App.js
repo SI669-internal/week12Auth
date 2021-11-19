6f6103ce82e6a857dfe7045934e0db95911599ed
Mark Newman <mwnewman@umich.edu>
2021-11-18 22:38:25 -0500

    logging in and out work

--- App.js
+++ App.js
@@ -5,12 +5,12 @@ import { FlatList, Modal, StyleSheet, Button, Alert,
 import { initializeApp, getApps } from 'firebase/app';
 import { 
   initializeFirestore, collection,  
-  onSnapshot,
+  onSnapshot, query,
   doc, addDoc, setDoc, updateDoc
 } from "firebase/firestore";
 import { getAuth, onAuthStateChanged,
   signInWithEmailAndPassword,
-  createUserWithEmailAndPassword
+  createUserWithEmailAndPassword, signOut
 } from "firebase/auth";
 
 import { firebaseConfig } from './Secrets';
@@ -29,123 +29,154 @@ export default function App() {
 
   const [users, setUsers] = useState([]);
   const [mode, setMode] = useState('login');
+  const [isAuthed, setIsAuthed] = useState(false);
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [displayName, setDisplayName] = useState('');
 
   useEffect(()=>{
-    const query = collection(db, 'users');
-    onSnapshot(query, qSnap => {
-      let userList = [];
-      qSnap.forEach(docSnap => {
-        let u = docSnap.data();
-        u.key = docSnap.id;
-        userList.push(u);
-      });
-      setUsers(userList);
-    })
+    let onSnapshotUnsub = undefined;
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        const query = collection(db, 'users');
+        onSnapshotUnsub = onSnapshot(query, qSnap => {
+          let userList = [];
+          qSnap.forEach(docSnap => {
+            let u = docSnap.data();
+            u.key = docSnap.id;
+            userList.push(u);
+          });
+          setUsers(userList);
+        });  
+      } else {
+        // logged out
+        if (onSnapshotUnsub) {
+          onSnapshotUnsub();
+          setUsers([]);
+        }
+      }
+    });
   }, []);
 
   return (
     <View style={styles.container}>      
-      <View style={styles.loginContainer}>
-        <View style={styles.sectionHeader}>
-          <Text style={styles.sectionHeaderText}>
-            Login:
-          </Text>
-        </View>
-        <View style={styles.loginRow}>
-          <View style={styles.loginLabelContainer}>
-            <Text style={styles.loginLabelText}>Email: </Text>
+      {auth.currentUser ? 
+        
+        <View style={styles.loginContainer}>
+          <View style={styles.sectionHeader}>
+            <Text style={styles.sectionHeaderText}>
+              Logged In!
+            </Text>
           </View>
-          <View style={styles.loginInputContainer}>
-            <TextInput 
-              style={styles.loginInputBox}
-              placeholder='enter email address' 
-              autoCapitalize='none'
-              spellCheck={false}
-              value={email}
-              onChangeText={(text)=>{setEmail(text)}}
-            />
-          </View>
-        </View>
+          <Button
+            title='Sign Out'
+            onPress={()=>signOut(auth)}
+          />
+        </View> 
+      
+        :
 
-        <View style={styles.loginRow}>
-          <View style={styles.loginLabelContainer}>
-            <Text style={styles.loginLabelText}>Password: </Text>
+        <View style={styles.loginContainer}>
+          
+          <View style={styles.sectionHeader}>
+            <Text style={styles.sectionHeaderText}>
+              Login:
+            </Text>
           </View>
-          <View style={styles.loginInputContainer}>
-            <TextInput 
-              style={styles.loginInputBox}
-              placeholder='enter password' 
-              secureTextEntry={true}
-              value={password}
-              onChangeText={(text)=>{setPassword(text)}}
-            />
-          </View>
-        </View>
-
-        {mode === 'signup' ? 
           <View style={styles.loginRow}>
             <View style={styles.loginLabelContainer}>
-              <Text style={styles.loginLabelText}>Display Name: </Text>
+              <Text style={styles.loginLabelText}>Email: </Text>
             </View>
             <View style={styles.loginInputContainer}>
               <TextInput 
                 style={styles.loginInputBox}
-                placeholder='enter display name' 
+                placeholder='enter email address' 
                 autoCapitalize='none'
                 spellCheck={false}
-                value={displayName}
-                onChangeText={(text)=>{setDisplayName(text)}}
+                value={email}
+                onChangeText={(text)=>{setEmail(text)}}
               />
             </View>
           </View>
-          :
-          <View>{/* Empty view, show nothing */}</View> 
-        }
 
-        <View style={styles.modeSwitchContainer}>
-          {mode === 'login' ?
-            <Text>New user? 
-              <Text 
-                onPress={()=>{setMode('signup')}} 
-                style={{color: 'blue'}}> Sign up </Text> 
-            instead!</Text>
-          :
-            <Text>Existing user? 
-            <Text 
-              onPress={()=>{setMode('login')}} 
-              style={{color: 'blue'}}> Log In </Text> 
-            instead!</Text>
+          <View style={styles.loginRow}>
+            <View style={styles.loginLabelContainer}>
+              <Text style={styles.loginLabelText}>Password: </Text>
+            </View>
+            <View style={styles.loginInputContainer}>
+              <TextInput 
+                style={styles.loginInputBox}
+                placeholder='enter password' 
+                secureTextEntry={true}
+                value={password}
+                onChangeText={(text)=>{setPassword(text)}}
+              />
+            </View>
+          </View>
+
+          {mode === 'signup' ? 
+            <View style={styles.loginRow}>
+              <View style={styles.loginLabelContainer}>
+                <Text style={styles.loginLabelText}>Display Name: </Text>
+              </View>
+              <View style={styles.loginInputContainer}>
+                <TextInput 
+                  style={styles.loginInputBox}
+                  placeholder='enter display name' 
+                  autoCapitalize='none'
+                  spellCheck={false}
+                  value={displayName}
+                  onChangeText={(text)=>{setDisplayName(text)}}
+                />
+              </View>
+            </View>
+            :
+            <View>{/* Empty view, show nothing */}</View> 
           }
-        </View>
 
-        <View style={styles.loginButtonRow}>
-          <Button
-            title={mode==='login'?'Log in':'Sign up'}
-            onPress={async ()=>{  
-              if (mode === 'login') {
-                try {
-                  await signInWithEmailAndPassword(auth, email, password);
-                  console.log('logged in user', email);
-                } catch(error) {
-                  Alert.alert("Log in Error", error.message,[{ text: "OK" }]);
-                }
-            } else {
-              try {
-                await createUserWithEmailAndPassword(auth, email, password);  
-                console.log('created user', email);
-              } catch(error) {
-                Alert.alert("Sign Up Error", error.message,[{ text: "OK" }]);
-              }
+          <View style={styles.modeSwitchContainer}>
+            {mode === 'login' ?
+              <Text>New user? 
+                <Text 
+                  onPress={()=>{setMode('signup')}} 
+                  style={{color: 'blue'}}> Sign up </Text> 
+              instead!</Text>
+            :
+              <Text>Existing user? 
+              <Text 
+                onPress={()=>{setMode('login')}} 
+                style={{color: 'blue'}}> Log In </Text> 
+              instead!</Text>
             }
-            setEmail('');
-            setPassword('');
-          }}
-          />
+          </View>
+
+          <View style={styles.loginButtonRow}>
+            <Button
+              title={mode==='login'?'Log in':'Sign up'}
+              onPress={async ()=>{  
+                if (mode === 'login') {
+                  try {
+                    await signInWithEmailAndPassword(auth, email, password);
+                    console.log('logged in user', email);
+                  } catch(error) {
+                    Alert.alert("Log in Error", error.message,[{ text: "OK" }]);
+                  }
+              } else {
+                try {
+                  await createUserWithEmailAndPassword(auth, email, password);  
+                  console.log('created user', email);
+                } catch(error) {
+                  Alert.alert("Sign Up Error", error.message,[{ text: "OK" }]);
+                }
+              }
+              setEmail('');
+              setPassword('');
+            }}
+            />
+
+          </View>
         </View>
-      </View>
+      }
 
       <View style={styles.listContainer}>
         <View style={styles.sectionHeader}>
